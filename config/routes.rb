b1106100ce78749f41f09f396d5100a34a7da7e4
KateSTento <bktento@skymesh.com.au>
2016-09-06 13:44:08 +1000

    added User controller and intial new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+  get "users/new" => "users#new"
+
   root 'static_pages#home'
   
-  get 'static_pages/home'
+  get "home" => "static_pages#home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
