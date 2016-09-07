2fca8f122c72566cd9e0163751dddc42ffd80173
KateSTento <bktento@skymesh.com.au>
2016-09-07 12:27:51 +1000

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
-        redirect_to @user, notice: "Thanks for signing up!"
+        flash[:success] = "Thanks for signing up!"
+        redirect_to @user
       else
         render :new
       end
