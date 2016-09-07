399b0c6ed199e39da2cd232747d130913f442698
KateSTento <bktento@skymesh.com.au>
2016-09-07 12:18:51 +1000

    add working user new, edit, update actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
-  get "users/new" => "users#new"
-
   root 'static_pages#home'
-  
+  get "users/new" => "users#new"
   get "home" => "static_pages#home"
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
