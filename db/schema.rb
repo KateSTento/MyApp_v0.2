6c5f2974cb8fe006791c10f2a97e78ed3cfb2ab9
KateSTento <bktento@skymesh.com.au>
2016-09-11 10:11:19 +1000

    password reset feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908114409) do
+ActiveRecord::Schema.define(version: 20160910222215) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20160908114409) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
